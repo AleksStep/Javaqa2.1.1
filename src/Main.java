00febde10015cde5e0889adfe6336946bec6ab9a
AleksStep <aleks.step.kz@gmail.com>
2023-05-14 16:17:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 111;
+        int milsConst = 20;
+        int bonus = ticketprice / milsConst;
+        System.out.println("Бонус: " + bonus);
+
+    }
+}
